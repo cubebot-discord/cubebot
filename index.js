760bb8a03dc66e263a5823f80b4e37ad5a806a01
Matt <mmszek3000@gmail.com>
2018-03-30 12:06:23 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const Commando = require('discord.js-commando')
+const sqlite = require('sqlite')
+const path = require('path')
+const config = require('./config')
+
+const client = new Commando.Client({
+  owner: config.owner,
+  commandPrefix: config.prefix || '=>',
+  invite: config.invite
+})
+
+client
+  .setProvider(
+    sqlite
+      .open(path.join(__dirname, 'settings.sqlite3'))
+      .then(db => new Commando.SQLiteProvider(db))
+  )
+  .catch(console.error)
+
+client.registry
+  .registerGroups([])
+  .registerDefaults()
+  .registerCommandsIn(path.join(__dirname, 'commands'))
+
+client.on('ready', () => {
+  console.log('Connected')
+  console.log('Logged in as:')
+  console.log(`${client.user.tag} - (${client.user.id})`)
+  client.user.setActivity(`@${client.user.username} help`)
+})
+
+client.login(config.token)
